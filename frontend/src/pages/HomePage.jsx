98815d823c2d127e8261e348fc567316b0a68370
Daniella Toth <daniellatoth@me.com>
2025-12-21 21:22:02 -0500

    create hero section for main page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
     return (
-      <section>
-        <h1>tunetwin</h1>
-        <p>feel the tune, find your match.</p>
+      <section className="hero">
+        <div className="hero-bg"></div>
+        <div className="hero-content">
+          <h1>tunetwin</h1>
+          <p>feel the tune, find your match.</p>
+
+          <Link to="/search" className="hero-button">
+            search your song
+          </Link>
+        </div>
       </section>
     );
   }
